9555803146134e7333248834dd850f5a0f4f5ed0
ana-tchila <ana.tchiladze@warwick.ac.uk>
2026-04-28 21:59:00 +0100

    Add graph implementation with locations and connections
    Implemented a graph structure for the game with locations and connections.

--- MAIN.py
+++ MAIN.py
@@ -15,7 +15,6 @@ class Location:
         self.connections = {} #Dictionary to hold the connections (edges) 
         self.choices = [] #List of choices at the location 
 
-
 #Graph that the user will be moving through 
 
 class Graph: 
@@ -33,4 +32,38 @@ class Graph:
 		""" Method to add a connection(edge) between two locations on the graph.
 		It does this by adding to_location to the connections of the from_location"""
 		self.locations[location.name].connections[location.name] = to_location 
+
+#Implementation of the Graph 
+galaxy = Graph()
+
+#Adding the Nodes 
+start = Location( 'Start Point', 'Oooops the GPS broke down') 
+garage = Location( 'Garage', 'Shell plc gas station, where people can recharge their vehicles and get their vehicles fixed') 
+diner = Location( 'Diner', 'A Diner that offers more food: community') 
+shop = Location( 'Antique Shop', 'Every item has its use - find what is useful for you') 
+parade = Location( 'Alien Parade', 'Fun like you have never experienced before, join the alien king on his ship')
+destination = Location('Roupell Street SE1', 'House of Brad Cooper') 
+
+#Adding Nodes to the Graph 
+galaxy.add_location(starting) 
+galaxy.add_location(garage) 
+galaxy.add_location(diner) 
+galaxy.add_location(shop) 
+galaxy.add_location(parade) 
+galaxy.add_location(destination) 
+
+#Creating the edges 
+galaxy.add_connection(starting, garage) 
+galaxy.add_connection(starting, diner) 
+galaxy.add_connection(garage, diner) 
+galaxy.add_connection(diner, shop) 
+galaxy.add_connection(diner, parade) 
+galaxy.add_connection(shop, garage) 
+galaxy.add_connection(parade, destination) 
+galaxy.add_connection(garage, destination) 
+
+
+
+
+
 
