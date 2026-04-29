38620220ee38870bbfa89e07a434e7f0197a94d4
Mohammed <mmsaljabri2006@gmail.com>
2026-04-29 15:05:09 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,20 @@
+import pygame
 
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+font = pygame.font.Font(None, size=40)
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+
+    text = font.render("Intergalactic Delivery Guy", True, (0, 0, 0))
+    screen.blit(text, (350, 280))
+
+    pygame.display.flip()
