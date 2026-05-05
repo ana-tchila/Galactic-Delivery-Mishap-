c8c39bf727cdea1c379e8cc45aa9622aa6b13cee
latefa <alamoudilatefa@gmail.com>
2026-05-05 08:05:20 +0100

    Create garage.py
    Implemented a GPS code guessing game with lives and score tracking. The game allows players to guess a randomly generated three-digit GPS code with limited attempts.

---
+++ garage.py
@@ -0,0 +1,46 @@
+import random 
+import time 
+import os
+
+live = 3
+score = 0 
+attempts = 3 
+
+gps_code = str(random.radint(100, 999)) #generate threee didgit num randomly
+
+print("GPS code:", gps_code)
+time.sleep(3)
+
+if os.name == "nt"
+  os.system("cls")
+else:
+  os.system("clear")
+# I HAVE CHANGED THIS IF STATEMENT FROM THIS print("\n"* 100}
+# THEY ARE BOTH TO CLEAR EVERYTHING FROM THE SCREEN 
+# THE ONE I STUCK WITH IS MORE OFFICIAL - BY CHECKING THE COMPUTER'S OS SYSTEM - nt = windows
+# TGE PRINT STATEMENT IS JUST PRNTING EMPTY LINES 100 TIMES
+
+
+while attempts > 0:
+  amswer = input("Enter GPS code: ").strip()  #strip = strips the answer only, without any spaces = so one python compares thr generated num and the inputted text there is no error 
+
+  if answer == gps_code:
+    score = score + 1
+    result = "Congratulations, You Won the Game"
+    break
+  else:
+    lives = lives - 1
+    attempts = attempts -1  
+
+      if lives == 0:
+        result = "Lost in Garage"
+
+# FOR OTHER CODE SECTIONS 
+# if attempts = 0 and lives > 0:
+#   result = "Lost in Garage"
+
+print(result)
+print("Score:", score)
+print("Lives:", lives)
+
+  
