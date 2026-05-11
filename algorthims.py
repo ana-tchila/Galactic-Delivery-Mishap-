b1842dbcb7cc6f8325b62c5987e68f6f674eaf14
Mohammed <mmsaljabri2006@gmail.com>
2026-05-11 12:34:18 +0100

    Update algorthims.py

--- algorthims.py
+++ algorthims.py
@@ -36,21 +36,21 @@ class Inventory:
         return search(self.items, item) is not None
 
 
-shop_item_description = {"shopkeeper" : ("Welcome to my humble shop, starngar!", "You can find things from all around the universe here!"),
-"GPS": ( "Is this what you are looking for?", "Luckily, some guy sold me his old bike scraps, and this is one of the parts which are in good condition", "It looks rusty and old, but I assure you, it works like it is brand new!"), 
-"Raygun": ("A buddy from your planet with a funny accent called Richtofen sold me this weapon", "He told me it is one of his best creations or something....", "Yeah, it looks as weird as him") }
+shop_item_description = {"Shopkeeper" : ("Welcome to my humble shop, starngar!" "You can find things from all around the universe here!"),
+"GPS": ( "Is this what you are looking for?" "Luckily, some guy sold me his old bike scraps, and this is one of the parts which are in good condition" "It looks rusty and old, but I assure you, it works like it is brand new!"), 
+"Raygun": ("A buddy from your planet with a funny accent called Richtofen sold me this weapon" "He told me it is one of his best creations or something...." "Yeah, it looks as weird as him") }
 
 def search_shop(shop_choices, item_wanted, player_inventory):
     index = search(shop_choices.choices, item_wanted)
+    if index is None:
+         return f" You need to look for the '{item_wanted}'"
     offered_item = shop_choices.choices[index]
-    item_wanted = "GPS"
     descriptions = shop_item_description.get(offered_item, "Woah buddy, you wish!")
-    if offered_item == "shopkeeper":
+    if offered_item == "Shopkeeper":
         return descriptions
-     
     if player_inventory.has(item_wanted):
-        return f"n/ you found the GPS part!"
-    else:
-        return f" This is not helpful."
-
-    
+        return f"{descriptions}\nYou don't need {offered_item}."
+    player_inventory.add_item(offered_item)
+    return f"{descriptions}\n\nYou take the {offered_item}."
+   
+  
