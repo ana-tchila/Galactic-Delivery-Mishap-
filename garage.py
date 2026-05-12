806049df7c8e313013d5f1a05dbca7f433077c15
ana-tchila <ana.tchiladze@warwick.ac.uk>
2026-05-12 18:38:04 +0100

    Resolved merge conflict

--- garage.py
+++ garage.py
@@ -25,38 +25,4 @@ def garage_gps_game():
 
 garage_gps_game()
 
-
-<<<<<<< HEAD
-  #while attempts > 0  and lives > 0:
-  #  answer = input("Enter GPS code: ").strip()  #strip = strips the answer only, without any spaces = so one python compares thr generated num and the inputted text there is no error 
-
-   # if answer == gps_code:
-      #score = score + 1
-      #3
-      #result = "Congratulations, You Won the Game!"
-     # break
-    #else:
-    #  lives = lives - 1
-   #   attempts = attempts -1  
-
-   #   if lives == 0:
-  #      result = "Lost in Garage"
- #       break
-
-  # FOR OTHER CODE SECTIONS 
-  # if attempts ==0 and lives > 0:
-  #   result = "Lost in Garage"
-
- # print(result)
-  #print("Score:", score)
-  #print("Lives:", lives)
-
-=======
-      if lives == 0:
-        result = "Lost in Garage"
-        break
->>>>>>> 795cea2d78c7649b2e27b58abd4e37f0f257d292
-
-
-
   
