637282c4be77b6477101bda7b336d237b49584cc
Mohammed <mmsaljabri2006@gmail.com>
2026-05-13 16:52:29 +0100

    inegrating the graph

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
 #ID:5752030
+
 import pygame
 from sys import exit
+from Graph import galaxy, starting, garage, police, diner, shop, parade, destination, celebration, Stack, bfs, make_connections_reciprocal
+
 
 pygame.init()
 pygame.mixer.init()
@@ -17,7 +20,7 @@ font = pygame.font.Font('font/Pixeltype.ttf', size=30)
 player = pygame.image.load('player/bike.png')
 smaller_player = pygame.transform.scale(player, (70, 70))
 player_x_pos = [0]
-current_screen = "menu"
+
 
 
 class Button:
@@ -55,47 +58,76 @@ def draw_menu():
     if player_x_pos[0] > 800:
         player_x_pos[0] = -70
     screen.blit(smaller_player, (player_x_pos[0], 200))
+def wrap_text(text, font, max_width):
+    words = text.split(' ')
+    lines = []
+    current_line = ''
+    for word in words:
+        test_line = current_line + word + ' '
+        if font.size(test_line)[0] <= max_width:
+            current_line = test_line
+        else:
+            lines.append(current_line.strip())
+            current_line = word + ' '
+    if current_line:
+        lines.append(current_line.strip())
+    return lines
 
 
-def draw_start():
-    #scene title
+
+def build_choice_buttons(location, y_position=300):
+    """Buttons for in-scene dialogue choices."""
+    buttons = []
+    if not location.choices:
+        return buttons
+    button_width = 500
+    button_height = 40
+    for i, choice_text in enumerate(location.choices.keys()):
+        x = (800 - button_width) // 2
+        y = y_position + i * 50
+        buttons.append(Button(x, y, button_width, button_height, choice_text))
+    return buttons 
+
+def build_connection_buttons(location, y_position=300):
+    """Buttons for navigating to connected locations."""
+    buttons = []
+    if not location.connections:
+        return buttons
+    button_width = 500
+    button_height = 40
+    for i, conn in enumerate(location.connections):
+        x = (800 - button_width) // 2
+        y = y_position + i * 50
+        buttons.append(Button(x, y, button_width, button_height, f"Go to {conn.name}"))
+    return buttons
+
+def draw_scene(location, choice_buttons, connection_buttons):
     screen.fill((0, 0, 0))
-    title_text = title_font.render("Starting point", False, (255, 255, 255))
+    title_text = title_font.render(location.name, False, (255, 255, 255))
     screen.blit(title_text, (320, 50))
-    pygame.draw.rect(screen, (255, 255, 255), (300, 80, 200, 5))
+    pygame.draw.rect(screen, (255, 255, 255), (300, 85, 200, 5))
 
-    #scene description
-    desc_text = font.render("You are an intergalactic space delivery driver.\n"
-	"You have just received an urgent food order to Roupell Street SE1.\n"
-    "Your GPS is calculating the fastest route.. . .  .\n"
-			"ERROR: GPS CONNECTION LOST.\n" 
-			"You must navigate through the galaxy on your own.", False, (255, 255, 255))
-    dec_rect = desc_text.get_rect(center=(400, 200))
-    pygame.draw.rect(screen, (255, 255, 255), (dec_rect.x - 10, dec_rect.y - 10, dec_rect.width + 20, dec_rect.height + 20), 2)
-    screen.blit(desc_text, dec_rect)
+    desc_text = font.render(location.description, False, (255, 255, 255))
+    desc_rect = desc_text.get_rect(center=(400, 200))
+    pygame.draw.rect(screen, (255, 255, 255), (desc_rect.x - 10, desc_rect.y - 10, desc_rect.width + 20, desc_rect.height + 20), 2)
+    screen.blit(desc_text, desc_rect)
 
-    #prompt
-    prompt_text = title_font.render("\nWhere do you want to go?", False, (255, 255, 255))
+    for button in choice_buttons:
+        button.draw(screen)
+    for button in connection_buttons:
+        button.draw(screen)
 
-    screen.blit(prompt_text, (230, 400))
-    garage_button.draw(screen)
-    diner_button.draw(screen) 
 
-def draw_garage():
-    screen.fill((0, 0, 0))
-    text = title_font.render("Garage Screen", False, (255, 255, 255))
-    screen.blit(text, (325, 300))
-
-def draw_diner():
-    screen.fill((0, 0, 0))
-    text = title_font.render("Diner Screen", False, (255, 255, 255))
-    screen.blit(text, (325, 300))
-
+current_screen = "menu"
+current_location = starting
+movement_history = Stack()
+current_dialogue = ""
+choice_buttons = build_choice_buttons(current_location)
+connection_buttons = build_connection_buttons(current_location)
 
 
 start_button = Button(300, 400, 200, 50, "Start Game")
-garage_button = Button(150, 480, 200, 50, "Go to Garage")
-diner_button = Button(420, 480, 200, 50, "Go to Diner")
+
 while running:
     
     for event in pygame.event.get():
@@ -108,22 +140,31 @@ while running:
                     current_screen = "game"
 
             if current_screen == "game":
-                if garage_button.is_clicked(event.pos):
-                        current_screen = "garage"
-                elif diner_button.is_clicked(event.pos):
-                    current_screen = "diner"
+                for button in choice_buttons:
+                    if button.is_clicked(event.pos):
+                        choice_text = button.text
+                        current_dialogue = current_location.choices[choice_text]
+                        choice_buttons = []
+                        connection_buttons = build_connection_buttons(current_location)
+                        break
 
+                for button in connection_buttons:
+                    if button.is_clicked(event.pos):
+                        dest_name = button.text.replace("Go to ", "")
+                        next_location = next((loc for loc in current_location.connections if loc.name == dest_name), None)
+                        if next_location:
+                            movement_history.push(current_location)
+                            current_location = next_location
+                            choice_buttons = build_choice_buttons(current_location)
+                            connection_buttons = build_connection_buttons(current_location)
+                            
+
+
+    
     if current_screen == "menu":
         draw_menu()
     elif current_screen == "game":
-        draw_start()
-    elif current_screen == "garage":
-        draw_garage()
-    elif current_screen == "diner":
-        draw_diner()
-
-    
-
+        draw_scene(current_location, choice_buttons, connection_buttons, current_dialogue)
 
     pygame.display.flip()
     clock.tick(60)
